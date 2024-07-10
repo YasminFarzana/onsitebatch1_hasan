d9de53481ae5b7f10d62325828eaf10ecba593df
lamia labib <farzanayasmin403@gmail.com>
2024-07-09 18:11:20 -0800

    test

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # onsitebatch1_hasan
 java, cucumber BDD, Gharkin, CI/CD, github, maven, git etc
+# Tools Required
+-Github Account
+-Install Git
+-java 17 or up
+-cucumber and selenium(add that inside the dependences)
+-install maven
+-IDE(intellj install for free)
+# List of Denpendences:https://mvnrepository.com/
+-seleniun-java
+-Cucumber-Testng
+Cucumber-java
+# Plugin from Intellj
+-Cumcumber for java
+-Gharkin
---
+++ src/test/java/practiceSelenium.java
@@ -0,0 +1,32 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class practiceSelenium {
+    public static void main(String[] args) {
+
+    WebDriver driver= new ChromeDriver();
+    driver.get("https://qa.taltektc.com/");
+
+
+    driver.findElement(By.linkText("Create new account")).click();
+
+    driver.findElement(By.name("firstName")).sendKeys("tanzila");
+    driver.findElement(By.name("lastName")).sendKeys("yasmin");
+    driver.findElement(By.name("email")).sendKeys("tanshuha13@gmail.com");
+    driver.findElement(By.name("password")).sendKeys("123456");
+    driver.findElement(By.name("confirmPassword")).sendKeys("123456");
+    driver.findElement(By.name("month")).click();
+    new Select(driver.findElement(By.name("month"))).selectByVisibleText("Nov");
+    driver.findElement(By.name("day")).click();
+    new Select(driver.findElement(By.name("day"))).selectByVisibleText("10");
+    driver.findElement(By.name("year")).click();
+    new Select(driver.findElement(By.name("year"))).selectByVisibleText("1993");
+    driver.findElement(By.id("female")).click();
+    driver.findElement(By.id("defaultCheck1")).click();
+    driver.findElement(By.xpath("//button[@type='submit']")).click();
+}
+
+}
+
